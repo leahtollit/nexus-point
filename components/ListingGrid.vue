93d980338735647b1ae50d7c14bead26accface5
leahtollit <leahtollit@gmail.com>
2023-02-16 10:21:24 +0000

    update card data

--- components/ListingGrid.vue
+++ components/ListingGrid.vue
@@ -31,13 +31,42 @@
 						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
           },
 					{
-            header: '2014 (64) Mercedes-Benz',
-						subheading: 'CLA 250e Coupe Shooting Break',
+            header: '2020 (20) Peugeot 208',
+						subheading: '1.2 PureTech 100 GT-Line',
 						price_per_month: '£550.90',
 						price: '£23,300',
+						advert_classification: 'New',
+						image: '/images/thumbnails/car-two.jpg',
+						tags: [ 'Petrol', 'Automatic', 'Hatchback']
+          },
+					{
+            header: '2019 (19) Renault Zoe',
+						subheading: 'i R135 Rapid Charge ZE50',
+						price_per_month: '£550.90',
+						price: '£23,300',
+						is_discounted: true,
+						new_price: '£23,300',
 						advert_classification: 'Used',
-						image: '/images/thumbnails/car-one.jpg',
-						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
+						image: '/images/thumbnails/car-three.jpg',
+						tags: [ '325 miles', 'EV', 'Automatic', 'Hatchback']
+          },
+					{
+            header: '2020 (20) Peugeot 208',
+						subheading: '1.2 PureTech 100 GT-Line',
+						price_per_month: '£550.90',
+						price: '£23,300',
+						advert_classification: 'New',
+						image: '/images/thumbnails/car-two.jpg',
+						tags: [ 'Petrol', 'Automatic', 'Hatchback']
+          },
+					{
+            header: 'EMPTY',
+						subheading: '1.2 PureTech 100 GT-Line',
+						price_per_month: '£550.90',
+						price: '£23,300',
+						advert_classification: 'New',
+						image: '/images/thumbnails/car-two.jpg',
+						tags: [ 'Petrol', 'Automatic', 'Hatchback']
           },
 					{
             header: '2014 (64) Mercedes-Benz',
@@ -49,13 +78,15 @@
 						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
           },
 					{
-            header: '2014 (64) Mercedes-Benz',
-						subheading: 'CLA 250e Coupe Shooting Break',
+            header: '2019 (19) Renault Zoe',
+						subheading: 'i R135 Rapid Charge ZE50',
 						price_per_month: '£550.90',
 						price: '£23,300',
+						is_discounted: true,
+						new_price: '£23,300',
 						advert_classification: 'Used',
-						image: '/images/thumbnails/car-one.jpg',
-						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
+						image: '/images/thumbnails/car-three.jpg',
+						tags: [ '325 miles', 'EV', 'Automatic', 'Hatchback']
           },
 					{
             header: '2014 (64) Mercedes-Benz',
@@ -67,13 +98,13 @@
 						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
           },
 					{
-            header: '2014 (64) Mercedes-Benz',
-						subheading: 'CLA 250e Coupe Shooting Break',
+            header: '2020 (20) Peugeot 208',
+						subheading: '1.2 PureTech 100 GT-Line',
 						price_per_month: '£550.90',
 						price: '£23,300',
-						advert_classification: 'Used',
-						image: '/images/thumbnails/car-one.jpg',
-						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
+						advert_classification: 'New',
+						image: '/images/thumbnails/car-two.jpg',
+						tags: [ 'Petrol', 'Automatic', 'Hatchback']
           },
 					{
             header: '2014 (64) Mercedes-Benz',
@@ -85,50 +116,26 @@
 						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
           },
 					{
-            header: '2014 (64) Mercedes-Benz',
-						subheading: 'CLA 250e Coupe Shooting Break',
+            header: '2020 (20) Peugeot 208',
+						subheading: '1.2 PureTech 100 GT-Line',
 						price_per_month: '£550.90',
 						price: '£23,300',
-						advert_classification: 'Used',
-						image: '/images/thumbnails/car-one.jpg',
-						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
+						advert_classification: 'New',
+						image: '/images/thumbnails/car-two.jpg',
+						tags: [ 'Petrol', 'Automatic', 'Hatchback']
           },
 					{
-            header: '2014 (64) Mercedes-Benz',
-						subheading: 'CLA 250e Coupe Shooting Break',
+            header: '2019 (19) Renault Zoe',
+						subheading: 'i R135 Rapid Charge ZE50',
 						price_per_month: '£550.90',
 						price: '£23,300',
+						is_discounted: true,
+						new_price: '£23,300',
 						advert_classification: 'Used',
-						image: '/images/thumbnails/car-one.jpg',
-						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
+						image: '/images/thumbnails/car-three.jpg',
+						tags: [ '325 miles', 'EV', 'Automatic', 'Hatchback']
           },
-					{
-            header: '2014 (64) Mercedes-Benz',
-						subheading: 'CLA 250e Coupe Shooting Break',
-						price_per_month: '£550.90',
-						price: '£23,300',
-						advert_classification: 'Used',
-						image: '/images/thumbnails/car-one.jpg',
-						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
-          },
-					{
-            header: '2014 (64) Mercedes-Benz',
-						subheading: 'CLA 250e Coupe Shooting Break',
-						price_per_month: '£550.90',
-						price: '£23,300',
-						advert_classification: 'Used',
-						image: '/images/thumbnails/car-one.jpg',
-						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
-          },
-					{
-            header: '2014 (64) Mercedes-Benz',
-						subheading: 'CLA 250e Coupe Shooting Break',
-						price_per_month: '£550.90',
-						price: '£23,300',
-						advert_classification: 'Used',
-						image: '/images/thumbnails/car-one.jpg',
-						tags: [ '32k miles', 'Hybrid', 'Manual', 'SUV']
-          }
+				
 			  ]
 			}
 		},
